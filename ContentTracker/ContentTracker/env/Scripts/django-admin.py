1c2d27459dded6d45c8bce750895ef58a9ed8c2f
Cray Jaeger <cray.jaeger1@gmail.com>
2018-11-21 15:56:29 -0800

    Create project

---
+++ ContentTracker/ContentTracker/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\cray\Documents\Code\Django\ContentTracker\ContentTracker\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
